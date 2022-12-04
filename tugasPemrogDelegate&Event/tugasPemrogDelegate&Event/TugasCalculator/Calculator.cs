1b3ef347826e3e4bfd32a2128a925f16bc373d05
Firresa Alif Nurrahman <89144223+firresaalif@users.noreply.github.com>
2022-12-04 22:01:43 +0700

    Add files via upload

---
+++ tugasPemrogDelegate&Event/tugasPemrogDelegate&Event/TugasCalculator/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// NAMA : FIRRESA ALIF NURRAHMAN NIM : 21.11.4104 KELAS : 21 IF 04
+
+namespace TugasCalculator
+{
+    public class Calculator
+    {
+        public int NilaiA { get; set; }
+        public int NilaiB { get; set; }
+        public string Hasil { get; set; }
+
+    }
+}
---
+++ tugasPemrogDelegate&Event/tugasPemrogDelegate&Event/TugasCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// NAMA : FIRRESA ALIF NURRAHMAN NIM : 21.11.4104 KELAS : 21 IF 04
+
+namespace TugasCalculator
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new formCalculator());
+        }
+    }
+}
